c122ba77413385595106f88b98eb145cbdfc75af
andres.iglesias <andres.iglesias@dgac.gob.cl>
2024-08-22 16:24:27 -0400

    # Integraciónc capa negocio formulario

---
+++ src/main/java/org/dgac/cl/negocio/FormularioNegocio.java
@@ -0,0 +1,36 @@
+package org.dgac.cl.negocio;
+
+import java.util.List;
+
+import org.dgac.cl.model.entity.Formulario;
+import org.dgac.cl.model.service.FormularioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormularioNegocio {
+
+    @Autowired FormularioService service;
+
+    public List<Formulario> findAll(){
+        return service.findAll();
+    }
+
+    public Formulario findById(Long id){
+        return service.findById(id);
+    }
+
+    public Formulario save(Formulario formulario){
+        return service.save(formulario);
+    }
+
+    public void deleteById(Long id) throws Exception{
+        Formulario formularioExistente = service.findById(id);
+        if(formularioExistente == null){
+            throw new Exception("Formulario no encontrado");
+        }
+        service.deleteById(id);
+    }
+
+}
