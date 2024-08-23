5bf8100d5358be1f0f1897eb4266a917750cb986
Orlando Jafet Tobar Diaz <orl.tobar@duocuc.cl>
2024-08-23 15:19:14 -0400

    D	ao para UsuarioUnidadAeroportuariaPerfil

---
+++ src/main/java/org/dgac/cl/model/dao/UsuarioUnidadAeroportuarioPerfilDAO.java
@@ -0,0 +1,8 @@
+package org.dgac.cl.model.dao;
+
+import org.dgac.cl.model.entity.UsuarioUnidadAeroportuariaPerfilId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioUnidadAeroportuarioPerfilDAO extends JpaRepository<UsuarioUnidadAeroportuariaPerfilId,Integer> {
+
+}
