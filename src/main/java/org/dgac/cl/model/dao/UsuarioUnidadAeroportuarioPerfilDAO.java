5898d080aaaf0be8701da5a2f464d6ed6df92ab2
Orlando Jafet Tobar Diaz <orl.tobar@duocuc.cl>
2024-08-23 15:33:07 -0400

    cambios

--- src/main/java/org/dgac/cl/model/dao/UsuarioUnidadAeroportuarioPerfilDAO.java
+++ src/main/java/org/dgac/cl/model/dao/UsuarioUnidadAeroportuarioPerfilDAO.java
@@ -1,8 +1,9 @@
 package org.dgac.cl.model.dao;
 
+import org.dgac.cl.model.entity.UsuarioUnidadAeroportuariaPerfil;
 import org.dgac.cl.model.entity.UsuarioUnidadAeroportuariaPerfilId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioUnidadAeroportuarioPerfilDAO extends JpaRepository<UsuarioUnidadAeroportuariaPerfilId,Integer> {
+public interface UsuarioUnidadAeroportuarioPerfilDAO extends JpaRepository<UsuarioUnidadAeroportuariaPerfil,UsuarioUnidadAeroportuariaPerfilId> {
 
 }
