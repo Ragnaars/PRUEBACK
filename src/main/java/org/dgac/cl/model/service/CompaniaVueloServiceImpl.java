8312b39e715454b38c76afe279d2ba67df761c69
Orlando Jafet Tobar Diaz <orl.tobar@duocuc.cl>
2024-09-05 15:08:47 -0400

    fix: dependencia circular por servicio importado en servicio
    Se cambio la importacion de service a dao.

--- src/main/java/org/dgac/cl/model/service/CompaniaVueloServiceImpl.java
+++ src/main/java/org/dgac/cl/model/service/CompaniaVueloServiceImpl.java
@@ -2,6 +2,7 @@ package org.dgac.cl.model.service;
 
 import java.util.List;
 
+import org.dgac.cl.model.dao.CompaniaVueloDao;
 import org.dgac.cl.model.entity.CompaniaVuelo;
 import org.dgac.cl.model.entity.CompaniaVueloId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,24 +11,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class CompaniaVueloServiceImpl implements CompaniaVueloService {
 
-    @Autowired CompaniaVueloService service;
+    @Autowired CompaniaVueloDao dao;
 
     @Override
     public List<CompaniaVuelo> findByCompaniaAerea(Integer companiaAerea) {
 
-        return service.findByCompaniaAerea(companiaAerea);
+        return dao.findByCompaniaAerea(companiaAerea);
     }
 
     @Override
     public CompaniaVuelo findById(CompaniaVueloId id) {
 
-        return service.findById(id);
+        return dao.findById(id).orElse(null);
     }
 
     @Override
     public CompaniaVuelo save(CompaniaVuelo companiaVuelo) {
 
-        return service.save(companiaVuelo);
+        return dao.save(companiaVuelo);
     }
 
 }
